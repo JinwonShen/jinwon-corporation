7e971449ab339b3900a29c5ac55c0445afb142ba
JinwonShen <bosv031999@gmail.com>
2025-04-21 16:24:03 +0900

    프로젝트 세팅

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <footer>
+      <h1>진원상사</h1>
+    </footer>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <header>
+      <h1>진원상사</h1>
+    </header>
+  )
+}
