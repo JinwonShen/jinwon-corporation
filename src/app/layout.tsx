f736328fb2d8e045248ea01a0f47904e97a4ec18
JinwonShen <bosv031999@gmail.com>
2025-04-21 18:29:16 +0900

    font, color 변수로 정의

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,24 @@
 import type { Metadata } from "next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import "@/styles/globals.scss"
+import "@/styles/globals.scss";
 
 export const metadata: Metadata = {
-  title: '진원상사',
-  description: '비닐 가공 23년, 정직과 신뢰로 만들어낸 품질',
+  title: "진원상사",
+  description: "비닐 가공 23년, 정직과 신뢰로 만들어낸 품질",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Gothic+A1:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <Header />
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import styles from "../styles/page.module.scss";
 export default function Home() {
   return (
     <div className={styles.wrapper}>
-      <section className={styles.swiper}></section>
+      <section className={styles.swiper}>
+        <div>img</div>
+      </section>
     </div>
   );
 }
