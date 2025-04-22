4fb1a116fdfd2150f54753a3c9fcc72874023b09
JinwonShen <bosv031999@gmail.com>
2025-04-22 21:33:48 +0900

    framer motion 적용 및 swiper 스타일적용

--- src/components/SwiperContainer.tsx
+++ src/components/SwiperContainer.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import { motion } from "framer-motion";
 import "swiper/css"; // 기본 스타일
 import "swiper/css/navigation"; // 사용하면 navigation 버튼용
 import "swiper/css/pagination"; // 페이지네이션 동그라미용
 
-import { Autoplay, Pagination } from "swiper/modules";
+import { useState } from "react";
 import Image from "next/image";
 import styles from "../styles/SwiperContainer.module.scss";
 
 type SlideData = {
+  id: string;
   title: string;
   subTitle: string;
   image: string;
@@ -17,16 +20,19 @@ type SlideData = {
 
 const slideData: SlideData[] = [
   {
+    id: "slide01",
     title: "정직과 신뢰로 만들어낸 품질",
     subTitle: "가공 경력 23년,",
     image: "/jinwon(1).jpeg",
   },
   {
+    id: "slide02",
     title: "믿고 맡길 수 있는 파트너",
     subTitle: "맞춤 제작부터 대량 생산까지",
     image: "/jinwon(2).jpeg",
   },
   {
+    id: "slide03",
     title: "믿을 수 있는 품질, 정확한 납기",
     subTitle: "합리적인 가격",
     image: "/jinwon(3).jpeg",
@@ -34,6 +40,8 @@ const slideData: SlideData[] = [
 ];
 
 export default function SwiperContainer() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <div className={styles.swiperContainer}>
       <Swiper
@@ -44,6 +52,7 @@ export default function SwiperContainer() {
           clickable: true,
           renderBullet: (index, className) => `<span class="custom-bullet ${className}"></span>`,
         }}
+        onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
         slidesPerView={1}
         autoplay={true}
         speed={1000}
@@ -51,11 +60,17 @@ export default function SwiperContainer() {
       >
         {slideData.map((slide, i) => {
           return (
-            <SwiperSlide key={i} className={styles.swiperSlide}>
-              <div className={styles.textCont}>
+            <SwiperSlide key={slide.id} className={styles.swiperSlide}>
+              <motion.div
+                key={slide.id}
+                className={styles.textCont}
+                initial={{ opacity: 0, y: 30 }}
+                animate={activeIndex === i ? { opacity: 1, y: 0 } : { opacity: 0, y: 30 }}
+                transition={{ duration: 1, delay: 0.3, ease: "easeOut" }}
+              >
                 <span>{slide.subTitle}</span>
                 <h1>{slide.title}</h1>
-              </div>
+              </motion.div>
               <div className={styles.imgCont}>
                 <Image
                   src={slide.image}
