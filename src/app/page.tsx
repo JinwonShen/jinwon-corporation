f7c0a16b503f630e70b29ac933b2840d9556fb42
JinwonShen <bosv031999@gmail.com>
2025-04-23 14:55:40 +0900

    swiper 스타일 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,3 +18,58 @@ export default function Home() {
     </div>
   );
 }
+
+/**
+ * 
+ * #mainVisual .pager {
+  position: absolute;
+  left: 50%;
+  bottom: 70px;
+  z-index: 101;
+  transform: translateX(-50%);
+  width: max-content;
+}
+#mainVisual .pager span {
+  opacity: 0.3;
+  position: relative;
+  width: 29px;
+  height: 29px;
+  margin: 0 5px;
+  background-color: transparent;
+  transition: all 0.3s;
+}
+#mainVisual .pager span.on {
+  opacity: 1;
+}
+#mainVisual .pager span:before {
+  opacity: 0;
+  display: block;
+  content: "";
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 45px;
+  height: 45px;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  border-radius: 50%;
+  transition: all 0.3s;
+}
+#mainVisual .pager span.on:before {
+  opacity: 1;
+  width: 24px;
+  height: 24px;
+}
+#mainVisual .pager span:after {
+  display: block;
+  content: "";
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 7px;
+  height: 7px;
+  border-radius: 50%;
+  background: #fff;
+}
+ */
